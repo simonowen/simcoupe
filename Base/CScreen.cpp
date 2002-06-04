ac457de2c1bf30defca9ae0d589740f7bbf4f31c
Simon Owen <simon.owen@simcoupe.org>
2002-06-04 01:03:45 +0000

    Added clipping support, which apply to all text and graphic functions.  This allows the GUI components to overlap the screen edges, and to clip to individual windows where necessary.
    Added Plot, DrawLine, FrameRect and DrawImage methods, as used mainly by the GUI.  Also added support for multiple fonts, including bold support by overlapped printing.
    
    Removed opaque and fixed width font drawing, which are no longer required by the debugger (it'll used the new GUI instead).

--- Base/CScreen.cpp
+++ Base/CScreen.cpp
@@ -20,7 +20,7 @@
 
 // Notes:
 //  The SAM screen is stored with 1 byte holding the palette colour used
-//  for each screen pixel, regardless of the screen mode.
+//  for each screen pixel, regardless of the screen mode. (0,0) is top-left.
 //
 //  On-screen text and graphics are always drawn in high resolution mode
 //  (double with width of low), and any existing line data is simply
@@ -32,7 +32,8 @@
 #include "Font.h"
 
 
-const unsigned int CHAR_WIDTH = 8, CHAR_HEIGHT = 8;     // Character cell dimensions
+int nClipX, nClipY, nClipWidth, nClipHeight;    // Clip box for any screen drawing
+FONT* pFont = &sNewFont;
 
 
 CScreen::CScreen (int nWidth_, int nHeight_)
@@ -43,11 +44,13 @@ CScreen::CScreen (int nWidth_, int nHeight_)
     m_pbFrame = new BYTE [m_nPitch * m_nHeight];
     m_pfHiRes = new bool [m_nHeight];
 
-    // Create the look-up table from line number to start of
+    // Create the look-up table from line number to start of screen line
     m_ppbLines = new BYTE* [m_nHeight];
     for (int i = 0 ; i < m_nHeight ; i++)
         m_ppbLines[i] = m_pbFrame + (m_nPitch * i);
 
+    // Set default clipping (full screen) and clear the screen
+    SetClip();
     Clear();
 }
 
@@ -58,9 +61,7 @@ CScreen::~CScreen ()
     if (m_ppbLines) { delete m_ppbLines; m_ppbLines = NULL; }
 }
 
-
 ////////////////////////////////////////////////////////////////////////////////
-
 
 // Return the address of a high-res version of a line, converting from lo-res if necessary
 BYTE* CScreen::GetHiResLine (int nLine_, int nWidth_/*=WIDTH_BLOCKS*/)
@@ -94,140 +95,204 @@ BYTE* CScreen::GetHiResLine (int nLine_, int nWidth_/*=WIDTH_BLOCKS*/)
     return pbLine;
 }
 
-
 void CScreen::Clear ()
 {
     memset(m_pbFrame, 0, m_nPitch * m_nHeight);
     memset(m_pfHiRes, 0, m_nHeight * sizeof(bool));
 }
 
+////////////////////////////////////////////////////////////////////////////////
 
+void CScreen::SetClip (int nX_/*=0*/, int nY_/*=0*/, int nWidth_/*=0*/, int nHeight_/*=0*/)
+{
+    if (!nWidth_) nWidth_= m_nPitch;
+    if (!nHeight_) nHeight_= m_nHeight;
+
+    if ((nClipX = nX_) < 0) { nWidth_  += nX_; nClipX = 0; }
+    if ((nClipY = nY_) < 0) { nHeight_ += nY_; nClipY = 0; }
+
+    nClipWidth = (nClipX+nWidth_ > m_nPitch) ? m_nPitch - nClipX : nWidth_;
+    nClipHeight = (nClipY+nHeight_ > m_nHeight) ? m_nHeight - nClipY : nHeight_;
+}
+
+bool CScreen::Clip (int& rnX_, int& rnY_, int& rnWidth_, int& rnHeight_)
+{
+    // Limit the supplied region to the current clipping region
+    if (rnX_ < nClipX) { rnWidth_ -= nClipX-rnX_; rnX_ = nClipX; }
+    if (rnY_ < nClipY) { rnHeight_ -= nClipY-rnY_; rnY_ = nClipY; }
+
+    int r = nClipX+nClipWidth, b = nClipY+nClipHeight;
+    if (rnX_+rnWidth_ > r) rnWidth_ = r - rnX_;
+    if (rnY_+rnHeight_ > b) rnHeight_ = b - rnY_;
+
+    // Return if there's anything left to draw
+    return rnWidth_ > 0 && rnHeight_ > 0;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+void CScreen::Plot (int nX_, int nY_, BYTE bColour_)
+{
+    int nWidth = 1, nHeight = 1;
+
+    if (Clip(nX_, nY_, nWidth, nHeight))
+        GetHiResLine(nY_++)[nX_] = bColour_;
+}
+
+// Draw a line from horizontal or vertical a given point (no diagonal lines yet)
+void CScreen::DrawLine (int nX_, int nY_, int nWidth_, int nHeight_, BYTE bColour_)
+{
+    // Horizontal line?
+    if (nWidth_)
+    {
+        nHeight_ = 1;
+        if (Clip(nX_, nY_, nWidth_, nHeight_))
+            memset(GetHiResLine(nY_++) + nX_, bColour_, nWidth_);
+    }
+
+    // Vertical line
+    else if (nHeight_)
+    {
+        nWidth_ = 1;
+        if (Clip(nX_, nY_, nWidth_, nHeight_))
+            while (nHeight_--)
+                GetHiResLine(nY_++)[nX_] = bColour_;
+    }
+}
 
 // Draw a solid rectangle on the display
 void CScreen::FillRect (int nX_, int nY_, int nWidth_, int nHeight_, BYTE bColour_)
 {
-    // Negative coordinates are taken to be in from the right and/or bottom of the display
-    if (nX_ < 0) nX_ += m_nPitch - nWidth_;
-    if (nY_ < 0) nY_ += m_nHeight - nHeight_;
-
-    // Iterate through each line in the block
-    while (nHeight_--)
+    if (Clip(nX_, nY_, nWidth_, nHeight_))
     {
-        BYTE* pb = GetHiResLine(nY_++) + nX_;
-
-        // For small block this is probably faster than memset!
-        for (int nWidth = nWidth_ ; nWidth-- ; *pb++ = bColour_);
+        // Iterate through each line in the block
+        while (nHeight_--)
+            memset(GetHiResLine(nY_++) + nX_, bColour_, nWidth_);
     }
 }
 
-
-// Draw an opaque character at a given location using the specified foregound and background colours
-void CScreen::DrawOpaqueChar (BYTE bChar_, BYTE bInk_/*=0x7f*/, BYTE bPaper_/*=0x00*/)
+// Draw a rectangle outline
+void CScreen::FrameRect (int nX_, int nY_, int nWidth_, int nHeight_, BYTE bColour_)
 {
-    // Out-of-range characters will be shown as a space
-    if (bChar_ < ' ' || bChar_ > 128)
-        bChar_ = ' ';
-
-    for (int nLine = m_nY+CHAR_HEIGHT-1 ; nLine >= m_nY ; nLine--)
-        GetHiResLine(nLine);
-
-    const BYTE* pbData = &abFont[bChar_ - ' '][1];
-
-    for (int i = 0 ; i < (int)CHAR_HEIGHT ; i++)
-    {
-        BYTE* pLine = GetLine(m_nY+i) + m_nX;
-
-        BYTE bData = *pbData++;
-        pLine[0] = (bData & 0x80) ? bInk_ : bPaper_;
-        pLine[1] = (bData & 0x40) ? bInk_ : bPaper_;
-        pLine[2] = (bData & 0x20) ? bInk_ : bPaper_;
-        pLine[3] = (bData & 0x10) ? bInk_ : bPaper_;
-        pLine[4] = (bData & 0x08) ? bInk_ : bPaper_;
-        pLine[5] = (bData & 0x04) ? bInk_ : bPaper_;
-        pLine[6] = (bData & 0x02) ? bInk_ : bPaper_;
-        pLine[7] = (bData & 0x01) ? bInk_ : bPaper_;
-    }
-
-    // Advance a character, wrapping if necessary
-    if ((m_nX += 8) >= (m_nPitch - 8))
-    {
-        m_nY += 8;
-        m_nX = 0;
-    }
+    // Draw lines for top, left, right and bottom
+    DrawLine(nX_, nY_, nWidth_, 0, bColour_);
+    DrawLine(nX_, nY_, 0, nHeight_, bColour_);
+    DrawLine(nX_+nWidth_-1, nY_, 0, nHeight_, bColour_);
+    DrawLine(nX_, nY_+nHeight_-1, nWidth_, 0, bColour_);
 }
 
-
-// Draw an opaque string of characters at the specified character position
-void CScreen::DrawOpaqueString (int nX_, int nY_, const char* pcsz_, BYTE bInk_/*=0x7f*/, BYTE bPaper_/*=0x00*/)
+// Draw an image from a matrix of palette colours
+void CScreen::DrawImage (int nX_, int nY_, int nWidth_, int nHeight_, const BYTE* pcbData_, const BYTE* pcbPalette_)
 {
-    MoveTo(nX_, nY_);
+    // Return if the image is entirely clipped
+    int nX = nX_, nY = nY_, nWidth = nWidth_, nHeight = nHeight_;
+    if (!Clip(nX, nY, nWidth, nHeight))
+        return;
 
-    while (*pcsz_)
-        DrawOpaqueChar(*pcsz_++, bInk_, bPaper_);
+    // Draw the region within the clipping area
+    for (int y = nY ; y < (nY+nHeight) ; y++)
+    {
+        const BYTE* pcbImage = pcbData_ + (y-nY_)*nWidth_;
+        BYTE *pb = GetHiResLine(y);
+
+        for (int x = nX ; x < (nX+nWidth) ; x++)
+        {
+            BYTE b1 = pcbPalette_[pcbImage[x-nX_]];
+
+            if (b1)
+                pb[x] = b1;
+        }
+    }
 }
-
 
 // Draw a proportionally spaced string of characters at a specified pixel position
-void CScreen::DrawString (int nX_, int nY_, const char* pcsz_, BYTE bInk_)
+void CScreen::DrawString (int nX_, int nY_, const char* pcsz_, BYTE bInk_, bool fBold_)
 {
-    if (nX_ < 0) nX_ += m_nPitch - GetStringWidth(pcsz_);
-    if (nY_ < 0) nY_ += m_nHeight - CHAR_HEIGHT;
+    int nFrom = max(nClipY,nY_);
+    int nTo = nY_ + pFont->wHeight;
+    nTo = min(nClipY+nClipHeight-1, nTo);
 
-    MoveTo(nX_, nY_);
+    if (nFrom > nTo)
+        return;
 
-    // Ensure the lines containing the character is in hi-res
-    for (int i = CHAR_HEIGHT-1 ; i >= 0 ; i--)
-        GetHiResLine(m_nY+i);
+    // Ensure the lines containing the character are hi-res
+    for (int i = nFrom ; i < nTo ; i++)
+        GetHiResLine(i);
 
     // Iterate thru the characters in the string
     for (BYTE bChar ; (bChar = *pcsz_++) ; )
     {
-        // Out-of-range characters will be shown as a space
-        if (bChar < ' ' || bChar > 128)
-            bChar = ' ';
+        // Out-of-range characters will be shown as an underscore
+        if (bChar < pFont->bFirst || bChar > pFont->bLast)
+            bChar = CHAR_UNKNOWN;
 
         // Look up the font data for the character
-        const BYTE* pbData = &abFont[bChar - ' '][0];
+        const BYTE* pbData = pFont->pcbData + (bChar - pFont->bFirst) * pFont->wCharSize;
 
-        // Pick out the proportional spacing details, and step back by the space preceding the character
-        BYTE bSpace = *pbData++;
-        m_nX -= (bSpace >> 4);
+        // Pick up the spacing details, calculate the width and step back by the space preceding the character
+        int nWidth = *pbData++ & 0x0f;
 
-        for (int i = 0 ; i < (int)CHAR_HEIGHT ; i++)
+        // Only draw the character if the entire width fits inside the clipping area
+        // Smarter clipping will only slow it down further, and can be added later if necessary
+        if ((nX_ >= nClipX) && (nX_ < (nClipX+nClipWidth-nWidth)))
         {
-            BYTE* pLine = GetLine(m_nY+i) + m_nX;
+            BYTE* pLine = GetLine(nFrom) + nX_;
+            pbData += (nFrom - nY_);
 
-            BYTE bData = *pbData++;
-            if (bData & 0x80) pLine[0] = bInk_;
-            if (bData & 0x40) pLine[1] = bInk_;
-            if (bData & 0x20) pLine[2] = bInk_;
-            if (bData & 0x10) pLine[3] = bInk_;
-            if (bData & 0x08) pLine[4] = bInk_;
-            if (bData & 0x04) pLine[5] = bInk_;
-            if (bData & 0x02) pLine[6] = bInk_;
-            if (bData & 0x01) pLine[7] = bInk_;
+            for (int i = nFrom ; i < nTo ; pLine += m_nPitch, i += 1)
+            {
+                BYTE bData = *pbData++;
+
+                if (!fBold_)
+                {
+                    if (bData & 0x80) pLine[0] = bInk_;
+                    if (bData & 0x40) pLine[1] = bInk_;
+                    if (bData & 0x20) pLine[2] = bInk_;
+                    if (bData & 0x10) pLine[3] = bInk_;
+                    if (bData & 0x08) pLine[4] = bInk_;
+                    if (bData & 0x04) pLine[5] = bInk_;
+                    if (bData & 0x02) pLine[6] = bInk_;
+                    if (bData & 0x01) pLine[7] = bInk_;
+                }
+                else
+                {
+                    if (bData & 0x80) pLine[0] = pLine[1] = bInk_;
+                    if (bData & 0x40) pLine[1] = pLine[2] = bInk_;
+                    if (bData & 0x20) pLine[2] = pLine[3] = bInk_;
+                    if (bData & 0x10) pLine[3] = pLine[4] = bInk_;
+                    if (bData & 0x08) pLine[4] = pLine[5] = bInk_;
+                    if (bData & 0x04) pLine[5] = pLine[6] = bInk_;
+                    if (bData & 0x02) pLine[6] = pLine[7] = bInk_;
+                    if (bData & 0x01) pLine[7] = pLine[8] = bInk_;
+                }
+            }
         }
 
-        // Step back by the amount of space after the character
-        m_nX += CHAR_WIDTH - (bSpace & 0xf) + 1;
+        // Move to the next character position
+        nX_ += nWidth + 1 + fBold_;
     }
 }
 
-
 // Get the on-screen width required for a specified string if drawn proportionally
-int CScreen::GetStringWidth (const char* pcsz_)
+/*static*/ int CScreen::GetStringWidth (const char* pcsz_)
 {
     int nWidth = 0;
 
-    for (BYTE bChar ; (bChar = *pcsz_++) ; )
+    for (BYTE bChar ; bChar = *pcsz_++ ; )
     {
-        // Out-of-range characters will be drawn as a space
-        if (bChar < ' ' || bChar > 128)
-            bChar = ' ';
+        // Out-of-range characters will be drawn as an underscore
+        if (bChar < pFont->bFirst || bChar > pFont->bLast)
+            bChar = CHAR_UNKNOWN;
 
-        BYTE bExtents = abFont[bChar - ' '][0];
-        nWidth += (CHAR_WIDTH - (bExtents & 0xf) + 1 - (bExtents >> 4));
+        const BYTE* pChar = pFont->pcbData + (bChar - pFont->bFirst) * pFont->wCharSize;
+        nWidth += (*pChar & 0xf) + CHAR_SPACING;
     }
 
-    return nWidth;
+    // Return the width, not including the final space
+    return nWidth - CHAR_SPACING;
+}
+
+/*static*/ void CScreen::SetFont (FONT* pFont_)
+{
+    pFont = pFont_;
 }
