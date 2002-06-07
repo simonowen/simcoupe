bfa9c92608c55078c0672ab9adb637321eeec233
Simon Owen <simon.owen@simcoupe.org>
2002-06-07 09:55:28 +0000

    Added preliminary OpenGL support (disabled by default - see #define in SimCoupe.h to enable).  5:4 pixel mode is now disabled on startup unless running in OpenGL mode (which supports the necessary stretching).
    Added support for the extra GUI palette colours, which follow the normal SAM palette entries
    
    Changed C++ namespace to a class with static functions.

--- SDL/Video.cpp
+++ SDL/Video.cpp
@@ -18,45 +18,159 @@
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
+//  ToDo: possibly separate out the OpenGL and SDL code more?
+
 #include "SimCoupe.h"
 #include "Video.h"
 
-#include "Frame.h"
 #include "Display.h"
+#include "Frame.h"
+#include "GUI.h"
 #include "Options.h"
 #include "UI.h"
-#include "Util.h"
 
+const int N_TOTAL_COLOURS = N_PALETTE_COLOURS + N_GUI_COLOURS;
 
-DWORD aulPalette[N_PALETTE_COLOURS];                                            // SAM palette values in appropriate format for video surface
-WORD awY[N_PALETTE_COLOURS], awU[N_PALETTE_COLOURS], awV[N_PALETTE_COLOURS];    // YUV values pre-shifted for overlay surface
+// SAM RGB values in appropriate format, and YUV values pre-shifted for overlay surface
+DWORD aulPalette[N_TOTAL_COLOURS];
+WORD awY[N_TOTAL_COLOURS], awU[N_TOTAL_COLOURS], awV[N_TOTAL_COLOURS];
 
 SDL_Surface *pBack, *pFront, *pIcon;
 
 
-namespace Video
-{
 // Mask for the current SimCoupe.bmp image used as the SDL program icon
 static BYTE abIconMask[] =
 {
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x1f, 0xe0, 0x00,
-    0x00, 0x3f, 0xf8, 0x00, 0x00, 0x7f, 0xfc, 0x00, 0x00, 0xff, 0xfe, 0x00, 0x01, 0xff, 0xff, 0x00,
-    0x01, 0xff, 0xff, 0x00, 0x03, 0xff, 0xff, 0x80, 0x03, 0xff, 0xff, 0x80, 0x03, 0xff, 0xff, 0x80,
-    0x03, 0xff, 0xff, 0x80, 0x03, 0xff, 0xff, 0x80, 0x08, 0x1f, 0xf0, 0x20, 0x1c, 0x7f, 0xfc, 0x70,
-    0x3c, 0xff, 0xfe, 0x78, 0x7f, 0xff, 0xff, 0xfc, 0x7f, 0xff, 0xff, 0xfc, 0x3f, 0xff, 0xff, 0xf8,
-    0x03, 0xff, 0xff, 0x80, 0x03, 0xff, 0xff, 0x80, 0x03, 0xff, 0xff, 0x80, 0x03, 0xff, 0xff, 0x80,
-    0x03, 0xff, 0xff, 0x80, 0x00, 0xfc, 0x7e, 0x00, 0x01, 0xfe, 0xff, 0x00, 0x01, 0xfe, 0xff, 0x00,
-    0x01, 0xfe, 0xff, 0x00, 0x01, 0xfe, 0xff, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x01, 0x00, 0x00, 0x00, 0x1f, 0xe0, 0x00,
+    0x00, 0x3f, 0xf8, 0x00, 0x00, 0x7f, 0xfc, 0x00,
+    0x00, 0xff, 0xfe, 0x00, 0x01, 0xff, 0xff, 0x00,
+    0x01, 0xff, 0xff, 0x00, 0x03, 0xff, 0xff, 0x80,
+    0x03, 0xff, 0xff, 0x80, 0x03, 0xff, 0xff, 0x80,
+    0x03, 0xff, 0xff, 0x80, 0x03, 0xff, 0xff, 0x80,
+    0x08, 0x1f, 0xf0, 0x20, 0x1c, 0x7f, 0xfc, 0x70,
+    0x3c, 0xff, 0xfe, 0x78, 0x7f, 0xff, 0xff, 0xfc,
+    0x7f, 0xff, 0xff, 0xfc, 0x3f, 0xff, 0xff, 0xf8,
+    0x03, 0xff, 0xff, 0x80, 0x03, 0xff, 0xff, 0x80,
+    0x03, 0xff, 0xff, 0x80, 0x03, 0xff, 0xff, 0x80,
+    0x03, 0xff, 0xff, 0x80, 0x00, 0xfc, 0x7e, 0x00,
+    0x01, 0xfe, 0xff, 0x00, 0x01, 0xfe, 0xff, 0x00,
+    0x01, 0xfe, 0xff, 0x00, 0x01, 0xfe, 0xff, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
 };
 
 
+#ifdef USE_OPENGL
+
+GLuint dlist;
+GLuint auTextures[6];
+DWORD dwTextureData[6][256][256];
+
+
+void glBork (const char* pcsz_)
+{
+    GLenum error = glGetError();
+    if (error != GL_NO_ERROR)
+    {
+        char szError[64];
+        sprintf(szError, "%s: %#04x", pcsz_, (UINT)error);
+#ifdef _WINDOWS
+        MessageBox(NULL, szError, "glBork", MB_ICONSTOP);
+#else
+        fprintf(stderr, "%s\n", szError);
+#endif
+        exit(0);
+    }
+}
+
+void TestFormat (GLenum format, GLenum type)
+{
+    glTexImage2D(GL_PROXY_TEXTURE_2D, 0, GL_RGB, 64, 64, 0, format, type, NULL);
+
+    GLenum error = glGetError();
+    if (error)
+        TRACE("Format:%d type:%d  NOT supported\n");
+    else
+    {
+        GLint r, g, b, a;
+        r = g = b = a = 0;
+        glGetTexLevelParameteriv(GL_PROXY_TEXTURE_2D, 0, GL_TEXTURE_RED_SIZE, &r);
+        glGetTexLevelParameteriv(GL_PROXY_TEXTURE_2D, 0, GL_TEXTURE_GREEN_SIZE, &g);
+        glGetTexLevelParameteriv(GL_PROXY_TEXTURE_2D, 0, GL_TEXTURE_BLUE_SIZE, &b);
+        glGetTexLevelParameteriv(GL_PROXY_TEXTURE_2D, 0, GL_TEXTURE_ALPHA_SIZE, &a);
+
+        TRACE("Format:%d type:%d  supported as:  [%d,%d,%d,%d]\n", r, g, b, a);
+    }
+}
+
+void InitGL ()
+{
+    int nWidth = Frame::GetWidth(), nHeight = Frame::GetHeight();
+
+    glViewport(0, 0, GetOption(ratio5_4) ? (nWidth * 5)/4 : nWidth, nHeight << 1);
+
+    glEnable(GL_TEXTURE_2D);
+    glGenTextures(6,auTextures);
+
+    for (int i = 0 ; i < 6 ; i++)
+    {
+        glBindTexture(GL_TEXTURE_2D,auTextures[i]);
+
+        bool fFiltered = true;
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, fFiltered ? GL_LINEAR : GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, fFiltered ? GL_LINEAR : GL_NEAREST);
+
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 256, 256, 0, GL_RGBA, GL_UNSIGNED_BYTE, dwTextureData[i]);
+        glBork("glTexImage2D");
+
+        TestFormat(GL_RGB, GL_UNSIGNED_BYTE);
+        TestFormat(GL_RGBA, GL_UNSIGNED_BYTE);
+    }
+
+    dlist=glGenLists(1);
+    glNewList(dlist,GL_COMPILE);
+
+    glClearColor( 0.0f, 0.0f, 0.0f, 0.0f );
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    for (int yy = 0 ; yy < 2 ; yy++)
+    {
+        for (int xx = 0 ; xx < 3 ; xx++)
+        {
+            float flWidth = 2.0 * 256 / nWidth, flHeight = 256.0 / nHeight;
+            float flX = -1.0 + (2.0 * 256 * xx / nWidth), flY = -1.0 + (256.0 * yy / nHeight);
+
+            if (flHeight)
+            {
+                glBindTexture(GL_TEXTURE_2D, auTextures[3*yy + xx]);
+
+                glBegin(GL_QUADS);
+                glTexCoord2f(0.0,1.0); glVertex2f(flX,           flY + flHeight);
+                glTexCoord2f(0.0,0.0); glVertex2f(flX,           flY);
+                glTexCoord2f(1.0,0.0); glVertex2f(flX + flWidth, flY);
+                glTexCoord2f(1.0,1.0); glVertex2f(flX + flWidth, flY + flHeight);
+                glEnd();
+            }
+        }
+    }
+
+    glEndList();
+}
+
+#endif
+
 // function to initialize DirectDraw in windowed mode
-bool Init (bool fFirstInit_/*=false*/)
+bool Video::Init (bool fFirstInit_/*=false*/)
 {
     bool fRet = false;
 
     // The lack of stretching support means the SDL version currently lacks certain features, so force the options for now
     SetOption(scanlines, true);
+#ifndef USE_OPENGL
+    SetOption(ratio5_4, false);
+#endif
 
 
     Exit(true);
@@ -69,18 +183,35 @@ bool Init (bool fFirstInit_/*=false*/)
     {
         SDL_WM_SetIcon(pIcon = SDL_LoadBMP(OSD::GetFilePath("SimCoupe.bmp")), abIconMask);
 
-        CScreen* pScreen = Frame::GetScreen();
-        DWORD dwWidth = pScreen->GetPitch(), dwHeight = pScreen->GetHeight() << 1;
+        DWORD dwWidth = Frame::GetWidth(), dwHeight = Frame::GetHeight();
 
         // In 5:4 mode we'll stretch the viewing surfaces by 25%
         if (GetOption(ratio5_4))
             (dwWidth *= 5) >>= 2;
 
-        int nDepth = GetOption(fullscreen) ? GetOption(depth) : SDL_GetVideoInfo()->vfmt->BitsPerPixel;
+        const SDL_VideoInfo* pInfo = SDL_GetVideoInfo();
+        int nDepth = GetOption(fullscreen) ? GetOption(depth) : pInfo->vfmt->BitsPerPixel;
 
         // Should the surfaces be in video memory?  (they need to be for any hardware acceleration)
         DWORD dwOptions = (GetOption(surface) >= 2) ? SDL_HWSURFACE : 0;
 
+#ifdef USE_OPENGL
+            SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+
+            if (GetOption(fullscreen))
+                dwOptions |= SDL_FULLSCREEN;
+
+            if (!(pFront = SDL_SetVideoMode(dwWidth, dwHeight, nDepth, (dwOptions | SDL_HWPALETTE | SDL_HWSURFACE | SDL_OPENGL))))
+            {
+                char sz[128];
+                sprintf(sz, "SDL_SetVideoMode() failed: %s", SDL_GetError());
+                UI::ShowMessage(msgFatal, sz);
+            }
+
+            InitGL();
+            CreatePalettes();
+            fRet = UI::Init(fFirstInit_);
+#else
         // Full screen mode requires a display mode change
         if (!GetOption(fullscreen))
             pFront = SDL_SetVideoMode(dwWidth, dwHeight, nDepth, dwOptions | ((nDepth == 8) ? SDL_HWPALETTE : 0));
@@ -132,6 +263,7 @@ bool Init (bool fFirstInit_/*=false*/)
 
             fRet = UI::Init(fFirstInit_);
         }
+#endif
     }
 
     if (!fRet)
@@ -142,7 +274,7 @@ bool Init (bool fFirstInit_/*=false*/)
 }
 
 // Cleanup DirectX by releasing all the interfaces we have
-void Exit (bool fReInit_/*=false*/)
+void Video::Exit (bool fReInit_/*=false*/)
 {
     TRACE("-> Video::Exit(%s)\n", fReInit_ ? "reinit" : "");
 
@@ -158,48 +290,24 @@ void Exit (bool fReInit_/*=false*/)
 
 
 // Create whatever's needed for actually displaying the SAM image
-bool CreatePalettes (bool fDimmed_/*=false*/)
+bool Video::CreatePalettes (bool fDimmed_)
 {
-    bool fPalette = pBack->format->BitsPerPixel == 8;
-    UINT uBPP = pBack->format->BitsPerPixel;
+    SDL_Color acPalette[N_TOTAL_COLOURS];
+    bool fPalette = pBack && (pBack->format->BitsPerPixel == 8);
 
-    // To handle any 16-bit pixel format we need to build some look-up tables
-    WORD awRedTab[256], awGreenTab[256], awBlueTab[256];
-    if (uBPP == 15 || uBPP == 16)
+    fDimmed_ |= (g_fPaused && !g_fFrameStep) || GUI::IsActive() || (!g_fActive && GetOption(pauseinactive));
+    const RGBA *pSAM = IO::GetPalette(fDimmed_), *pGUI = GUI::GetPalette();
+
+    // Build the full palette from SAM and GUI colours
+    for (int i = 0; i < N_TOTAL_COLOURS ; i++)
     {
-        WORD wRMask = pBack->format->Rmask;
-        WORD wGMask = pBack->format->Gmask;
-        WORD wBMask = pBack->format->Bmask;
+        // Look up the colour in the appropriate palette
+        const RGBA* p = (i < N_PALETTE_COLOURS) ? &pSAM[i] : &pGUI[i-N_PALETTE_COLOURS];
+        BYTE bRed = p->bRed, bGreen = p->bGreen, bBlue = p->bBlue, bAlpha = p->bAlpha;
 
-        // Create the lookup table
-        for (DWORD dw = 0; dw < 256 ; dw++)
-        {
-            awRedTab[dw] = dw ? WORD(((DWORD(wRMask) * (dw+1)) >> 8) & wRMask) : 0;
-            awGreenTab[dw] = dw ? WORD(((DWORD(wGMask * (dw+1))) >> 8) & wGMask) : 0;
-            awBlueTab[dw] = dw ? WORD(((DWORD(wBMask) * (dw+1)) >> 8) & wBMask) : 0;
-        }
-    }
-
-    static const BYTE ab[] = { 0x00, 0x24, 0x49, 0x6d, 0x92, 0xb6, 0xdb, 0xff };
-
-    SDL_Color acPalette[N_PALETTE_COLOURS];
-
-    // Loop through SAM's full palette of 128 colours
-    for (int i = 0; i < N_PALETTE_COLOURS ; i++)
-    {
-        // Convert from SAM palette position to 3-bit RGB
-        BYTE bBlue  = ab[(i&0x01) << 1| ((i&0x10) >> 2) | ((i&0x08) >> 3)];
-        BYTE bRed   = ab[(i&0x02)     | ((i&0x20) >> 3) | ((i&0x08) >> 3)];
-        BYTE bGreen = ab[(i&0x04) >> 1| ((i&0x40) >> 4) | ((i&0x08) >> 3)];
-
-        if (fDimmed_)
-        {
-            bBlue = (BYTE)(int(bBlue) * 2 / 3);
-            bRed = (BYTE)(int(bRed) * 2 / 3);
-            bGreen = (BYTE)(int(bGreen) * 2 / 3);
-        }
-
-        if (!fPalette)
+        if (!pBack)
+            aulPalette[i] = (bAlpha << 24) | (bBlue << 16) | (bGreen << 8) | bRed;
+        else if (!fPalette)
             aulPalette[i] = SDL_MapRGB(pBack->format, bRed, bGreen, bBlue);
         else
         {
@@ -211,14 +319,16 @@ bool CreatePalettes (bool fDimmed_/*=false*/)
         }
     }
 
+    // If a palette is required, set it now
     if (fPalette)
-        SDL_SetPalette(pBack, SDL_LOGPAL|SDL_PHYSPAL, acPalette, 10, N_PALETTE_COLOURS);
+        SDL_SetPalette(pBack, SDL_LOGPAL|SDL_PHYSPAL, acPalette, PALETTE_OFFSET, N_TOTAL_COLOURS);
 
     // Because the pixel format may have changed, we need to refresh the SAM CLUT pixel values
     for (int c = 0 ; c < 16 ; c++)
         clut[c] = aulPalette[clutval[c]];
 
+    // Ensure the display is redrawn to reflect the changes
+    Display::SetDirty();
+
     return true;
 }
-
-}   // namespace Video
