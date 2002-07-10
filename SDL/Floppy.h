1260e38c0ea48949d19b942bbc0b2b0278083c9c
Simon Owen <simon.owen@simcoupe.org>
2002-07-10 23:00:08 +0000

    Added the new member functions and variables needed for the initial Linux implementation.

--- SDL/Floppy.h
+++ SDL/Floppy.h
@@ -35,28 +35,38 @@ class Floppy
 class CFloppyStream : public CStream
 {
     public:
-        CFloppyStream (const char* pcszStream_, bool fReadOnly_) : CStream(pcszStream_, fReadOnly_) { }
-        virtual ~CFloppyStream () { Close(); }
+        CFloppyStream (const char* pcszStream_, bool fReadOnly_=false)
+            : CStream(pcszStream_, fReadOnly_), m_nFloppy(-1), m_uTrack(0) { }
+        ~CFloppyStream () { Close(); }
 
     public:
-        static bool IsRecognised (const char* pcszStream_) { return false; }
+        static bool IsRecognised (const char* pcszStream_);
 
     public:
-        bool IsOpen () const { return false; }
+        void Close ();
+
+    public:
+        bool IsOpen () const { return m_nFloppy != -1; }
         bool Rewind () { return false; }
         size_t Read (void* pvBuffer_, size_t uLen_) { return 0; }
         size_t Write (void* pvBuffer_, size_t uLen_) { return 0; }
 
-        BYTE Read (int nSide_, int nTrack_, int nSector_, BYTE* pbData_, UINT* puSize_) { return RECORD_NOT_FOUND; }
-        BYTE Write (int nSide_, int nTrack_, int nSector_, BYTE* pbData_, UINT* puSize_) { return WRITE_PROTECT; }
+        BYTE Read (UINT uSide_, UINT uTrack_, UINT uSector_, BYTE* pbData_, UINT* puSize_);
+        BYTE Write (UINT uSide_, UINT uTrack_, UINT uSector_, BYTE* pbData_, UINT* puSize_);
 
         bool GetAsyncStatus (UINT* puSize_, BYTE* pbStatus_) { return false; }
         bool WaitAsyncOp (UINT* puSize_, BYTE* pbStatus_) { return false; }
         void AbortAsyncOp () { }
 
-    protected:
-        void Close () { }
         BYTE TranslateError () const;
+
+    protected:
+        bool Open ();
+        bool ReadWrite (bool fRead_, UINT uSide_, UINT uTrack_, UINT uSector_, BYTE* pbData_, UINT* puSize_);
+
+    protected:
+        int m_nFloppy;
+        UINT m_uTrack;
 };
 
 #endif  // FLOPPY_H
