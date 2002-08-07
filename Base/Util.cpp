5e2bc85b3f97a126b277fe5855e45d3c4af3f413
Simon Owen <simon.owen@simcoupe.org>
2002-08-07 17:11:13 +0000

    Changed to use delete[] when freeing BYTE arrays, to keep Valgrind happy.

--- Base/Util.cpp
+++ Base/Util.cpp
@@ -1,9 +1,9 @@
-// Part of SimCoupe - A SAM Coupé emulator
+// Part of SimCoupe - A SAM Coupe emulator
 //
 // Util.cpp: Logging, tracing, and other utility tasks
 //
-//  Copyright (c) 1996-2001  Allan Skillman
-//  Copyright (c) 1999-2001  Simon Owen
+//  Copyright (c) 1999-2002  Simon Owen
+//  Copyright (c) 1996-2002  Allan Skillman
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -64,7 +64,7 @@ void Util::Exit ()
 {
     CloseLog();
 
-    if (s_pszTrace) { delete s_pszTrace; s_pszTrace = NULL; }
+    if (s_pszTrace) { delete[] s_pszTrace; s_pszTrace = NULL; }
 }
 
 
