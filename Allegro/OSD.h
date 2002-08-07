957c9ab6afac89abd85c813f0ffa6eb7f48a7819
Simon Owen <simon.owen@simcoupe.org>
2002-08-07 19:47:56 +0000

    Renamed IsHidden() parameter, to avoid confusion - it's a path and not just a filename.

--- Allegro/OSD.h
+++ Allegro/OSD.h
@@ -57,7 +57,7 @@ public:
     static PROFILE_T GetProfileTime ();
     static const char* GetFilePath (const char* pcszFile_="");
     static const char* GetFloppyDevice (int nDrive_);
-    static bool IsHidden (const char* pcszFile_);
+    static bool IsHidden (const char* pcszPath_);
 
     static void DebugTrace (const char* pcsz_);
     static int FrameSync (bool fWait_=true);
