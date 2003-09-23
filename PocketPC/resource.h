5103f963ee2dc38dc7ca79e9a7362e6734bb6fdf
Simon Owen <simon.owen@simcoupe.org>
2003-09-23 22:42:48 +0000

    Initial import.

---
+++ PocketPC/resource.h
@@ -0,0 +1,92 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by SimCoupe.rc
+//
+#define IDI_MAIN                        1
+#define IDD_EXPORT                      107
+#define IDD_ABOUT                       108
+#define IDD_PAGE_PARALLEL               164
+#define IDD_PAGE_SOUND                  165
+#define IDD_PAGE_DISPLAY                166
+#define IDD_PAGE_SYSTEM                 168
+#define IDD_PAGE_INPUT                  169
+#define IDD_PAGE_MIDI                   170
+#define IDD_PAGE_MISC                   171
+#define IDD_PAGE_DRIVES                 172
+#define IDD_PAGE_FNKEYS                 173
+#define IDD_IMPORT                      178
+#define IDB_NORMAL                      185
+#define IDB_SHIFT                       186
+#define IDB_SYMBOL                      187
+#define IDB_SIMCOUPE                    191
+#define IDE_PAGE                        1001
+#define IDE_STATION_ID                  1002
+#define IDE_ROM                         1003
+#define IDC_ALT_FOR_CNTRL               1004
+#define IDE_FILE                        1005
+#define IDC_ALTGR_FOR_EDIT              1006
+#define IDE_OFFSET                      1007
+#define IDC_SPIN1                       1008
+#define IDR_BASIC_ADDRESS               1009
+#define IDR_PAGE_OFFSET                 1010
+#define IDE_ADDRESS                     1011
+#define IDE_LENGTH                      1012
+#define IDC_MOUSE_ENABLED               1013
+#define IDC_STEREO                      1014
+#define IDC_FREQ                        1015
+#define IDC_MAIN_MEMORY                 1016
+#define IDC_EXTERNAL_MEMORY             1017
+#define IDC_MIDI_OUT                    1018
+#define IDC_SAMBUS_CLOCK                1019
+#define IDC_SAMPLE_SIZE                 1020
+#define IDC_MIDI_IN                     1021
+#define IDC_KEYBOARD_MAPPING            1022
+#define IDC_PARALLEL_2                  1023
+#define IDC_PARALLEL_1                  1024
+#define IDC_PRINTERS                    1025
+#define IDC_SAASOUND_ENABLED            1026
+#define IDC_DALLAS_CLOCK                1027
+#define IDC_CLOCK_SYNC                  1028
+#define IDC_FRAMESKIP_AUTOMATIC         1029
+#define IDC_FRAMESKIP                   1030
+#define IDC_PROFILE                     1031
+#define IDC_MODE3                       1032
+#define IDC_STATUS                      1033
+#define IDC_DRIVE_LIGHTS                1034
+#define IDC_SYNC                        1035
+#define IDC_PAUSE_INACTIVE              1036
+#define IDC_FAST_RESET                  1037
+#define IDC_FULLSCREEN                  1038
+#define IDC_BEEPER                      1039
+#define IDC_PRINTER_ONLINE              1040
+#define IDC_MIDI                        1041
+#define IDC_LATENCY                     1042
+#define IDC_DRIVE1                      1043
+#define IDC_DRIVE2                      1044
+#define IDB_DELETE                      1045
+#define IDB_EDIT                        1046
+#define IDB_ADD                         1047
+#define IDL_FNKEYS                      1048
+#define IDS_FREQ                        1049
+#define IDS_SAMPLE_SIZE                 1050
+#define IDS_LATENCY                     1051
+#define IDC_SOUND_ENABLED               1052
+#define IDB_BROWSE                      1053
+#define IDS_LENGTH                      1054
+#define IDB_FLUSH_PRINT_JOB             1055
+#define IDC_TURBO_LOAD                  1056
+#define IDC_SENSITIVITY                 1057
+#define IDS_SENSITIVITY                 1058
+#define ID_HOMEPAGE                     1059
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        192
+#define _APS_NEXT_COMMAND_VALUE         40155
+#define _APS_NEXT_CONTROL_VALUE         1101
+#define _APS_NEXT_SYMED_VALUE           102
+#endif
+#endif
