6993d9a4efe4ce9d67b90f1f1c59d88e64f54dd2
Simon Owen <simon.owen@simcoupe.org>
2004-09-16 23:10:31 +0000

    Added a simpler method to evaluate expressions and determine whether there was a parsing error.  This can be used to add expression support to most inputs without much code.
    Added flags to pass to the expression parser, which can limit what is accepted.  Simple numeric expressions can choose to diasllow register values, functions, etc.
    
    Commented the public enums, which may be used by other modules.

--- Base/Expr.h
+++ Base/Expr.h
@@ -2,7 +2,7 @@
 //
 // Expr.h: Infix expression parsing and postfix evaluation
 //
-//  Copyright (c) 1999-2003  Simon Owen
+//  Copyright (c) 1999-2004  Simon Owen
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -32,9 +32,13 @@ EXPR;
 class Expr
 {
     public:
-        static EXPR* Compile (const char* pcsz_, char** ppcszEnd_=NULL);
+        enum { none=0x00, noRegs=0x01, noVars=0x02, noFuncs=0x04, simple=0x0f };    // Flags to limit expression scope
+
+    public:
+        static EXPR* Compile (const char* pcsz_, char** ppcszEnd_=NULL, int nFlags_=none);
         static void Release (EXPR* pExpr_);
         static int Eval (const EXPR* pExpr_);
+        static bool Eval (const char* pcsz_, int& nValue_, int nFlags_=none);
 
     public:
         static EXPR True, False, Counter;
@@ -46,11 +50,19 @@ class Expr
 };
 
 
+// Leave the enums public to allow some poking around the tokenised expressions by calling code
+
+// Token types
 enum { T_NUMBER, T_UNARY_OP, T_BINARY_OP, T_REGISTER, T_VARIABLE, T_FUNCTION };
 
+// Unary operators
 enum { OP_UMINUS, OP_UPLUS, OP_BNOT, OP_NOT, OP_DEREF, OP_PEEK, OP_DPEEK, OP_IN };
+
+// Binary operators
 enum { OP_AND, OP_OR, OP_BOR, OP_BXOR, OP_BAND, OP_EQ, OP_NE, OP_LT, OP_LE, OP_GE,
        OP_GT, OP_SHIFTL, OP_SHIFTR, OP_ADD, OP_SUB, OP_MUL, OP_DIV, OP_MOD };
+
+// Registers and (read-only) variables
 enum { REG_A, REG_F, REG_B, REG_C, REG_D, REG_E, REG_H, REG_L,
        REG_ALT_A, REG_ALT_F, REG_ALT_B, REG_ALT_C, REG_ALT_D, REG_ALT_E, REG_ALT_H, REG_ALT_L,
        REG_AF, REG_BC, REG_DE, REG_HL, REG_ALT_AF, REG_ALT_BC, REG_ALT_DE, REG_ALT_HL,
