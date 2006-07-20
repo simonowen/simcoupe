ce05c86278189edeaab686b85f059db569217eac
Simon Owen <simon.owen@simcoupe.org>
2006-07-20 20:07:30 +0000

    Added new stuff, removed old stuff  :-)

--- PocketPC/resource.h
+++ PocketPC/resource.h
@@ -15,20 +15,20 @@
 #define IDD_PAGE_DRIVES                 172
 #define IDD_PAGE_FNKEYS                 173
 #define IDD_IMPORT                      178
+#define IDD_PAGE_KEYMAP                 179
 #define IDB_NORMAL                      185
 #define IDB_SHIFT                       186
 #define IDB_SYMBOL                      187
 #define IDB_SIMCOUPE                    191
 #define IDI_DISK                        192
+#define IDB_JOYSTICK                    192
 #define IDE_PAGE                        1001
-#define IDE_STATION_ID                  1002
 #define IDE_ROM                         1003
 #define IDC_ALT_FOR_CNTRL               1004
 #define IDE_FILE                        1005
 #define IDC_ALTGR_FOR_EDIT              1006
 #define IDE_OFFSET                      1007
 #define IDC_KPMINUS_RESET               1007
-#define IDC_SPIN1                       1008
 #define IDC_SAM_FKEYS                   1008
 #define IDR_BASIC_ADDRESS               1009
 #define IDR_PAGE_OFFSET                 1010
@@ -36,18 +36,18 @@
 #define IDE_LENGTH                      1012
 #define IDC_MOUSE_ENABLED               1013
 #define IDC_STEREO                      1014
-#define IDC_FREQ                        1015
+#define IDC_MOUSE_SWAP23                1014
 #define IDC_MAIN_MEMORY                 1016
 #define IDC_EXTERNAL_MEMORY             1017
 #define IDC_MIDI_OUT                    1018
 #define IDC_SAMBUS_CLOCK                1019
-#define IDC_SAMPLE_SIZE                 1020
 #define IDC_MIDI_IN                     1021
 #define IDC_KEYBOARD_MAPPING            1022
 #define IDC_PARALLEL_2                  1023
 #define IDC_PARALLEL_1                  1024
 #define IDC_PRINTERS                    1025
 #define IDC_SAASOUND_ENABLED            1026
+#define IDC_FLUSHDELAY                  1026
 #define IDC_DALLAS_CLOCK                1027
 #define IDC_CLOCK_SYNC                  1028
 #define IDC_FRAMESKIP_AUTOMATIC         1029
@@ -72,8 +72,6 @@
 #define IDB_EDIT                        1046
 #define IDB_ADD                         1047
 #define IDL_FNKEYS                      1048
-#define IDS_FREQ                        1049
-#define IDS_SAMPLE_SIZE                 1050
 #define IDS_LATENCY                     1051
 #define IDC_SOUND_ENABLED               1052
 #define IDB_BROWSE                      1053
@@ -83,7 +81,22 @@
 #define IDC_SENSITIVITY                 1057
 #define IDS_SENSITIVITY                 1058
 #define ID_HOMEPAGE                     1059
+#define IDC_SAVE_PROMPT                 1059
 #define IDS_VERSION                     1060
+#define IDC_AUTOBOOT                    1101
+#define IDC_DOSBOOT                     1102
+#define IDE_DOSDISK                     1104
+#define IDS_DOSDISK                     1105
+#define IDS_FLUSHDELAY                  1106
+#define IDC_LEFT                        1110
+#define IDC_RIGHT                       1111
+#define IDC_UP                          1112
+#define IDC_DOWN                        1113
+#define IDC_START                       1114
+#define IDC_BUTTON1                     1115
+#define IDC_BUTTON3                     1116
+#define IDC_BUTTON2                     1118
+#define IDC_BUTTON4                     1119
 
 // Next default values for new objects
 // 
@@ -92,7 +105,7 @@
 #define _APS_NO_MFC                     1
 #define _APS_NEXT_RESOURCE_VALUE        193
 #define _APS_NEXT_COMMAND_VALUE         40155
-#define _APS_NEXT_CONTROL_VALUE         1101
+#define _APS_NEXT_CONTROL_VALUE         1112
 #define _APS_NEXT_SYMED_VALUE           102
 #endif
 #endif
