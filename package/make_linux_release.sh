4f94d1695313d678de41f17cbde31f0275063be5
Simon Owen <simon@simonowen.com>
2022-05-05 17:22:29 +0100

    Added .tar.gz archive for Linux builds
    This is a flattened version of the contents of the .deb archive. It gives
    a simple portable Linux build that should run on most modern distros.
    
    We now also statically link to zlib/bzip2 in portable builds, as runtime
    dynamic versions were found not to match in some cases.

--- package/make_linux_release.sh
+++ package/make_linux_release.sh
@@ -2,7 +2,7 @@
 set -e
 
 BUILD_DIR=build-linux
-mkdir -p ${BUILD_DIR} && pushd ${BUILD_DIR}
+mkdir -p "${BUILD_DIR}" && pushd "${BUILD_DIR}"
 
 cmake .. \
   -DCMAKE_FIND_LIBRARY_SUFFIXES=".a" \
@@ -16,6 +16,16 @@ ldd ./simcoupe
 readelf -d ./simcoupe | grep NEEDED
 
 cpack -V -G DEB
-DEB=`ls -t _packages/*.deb | head -1`
-dpkg --contents ${DEB} | cut -d' ' -f3-
-echo DEB: ${BUILD_DIR}/${DEB}
+DEBFILE=`ls -t _packages/*.deb | head -1`
+dpkg --contents "${DEBFILE}" | cut -d' ' -f3-
+
+BASE=$(basename "${DEBFILE}" .deb)
+TARFILE="${BASE}.tar.gz"
+mkdir -p "${BASE}" "${BASE}-temp"
+dpkg --fsys-tarfile "$DEBFILE" | tar -xf - -C "${BASE}-temp"
+find "${BASE}-temp" -type f -print0 | xargs -0 -I '{}' mv '{}' "${BASE}"
+tar -zvcf "${TARFILE}" "${BASE}"
+rm -rf "${BASE}" "${BASE}-temp"
+
+echo DEB: ${BUILD_DIR}/${DEBFILE}
+echo TAR: ${BUILD_DIR}/${TARFILE}
